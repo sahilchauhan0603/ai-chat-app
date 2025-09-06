3be74d065c283846004d8466aa28ac7ff34ee721
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2025-09-07 01:37:48 +0530

    changed the port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 9080,
   },
   plugins: [react()],
   resolve: {
